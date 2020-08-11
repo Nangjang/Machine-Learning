e66cf7529b31a79d0e248a03291fa28f7cea3467
Nangjang Subba <jintrade23@gmail.com>
2020-08-11 13:24:30 +0545

    Added Standard-Deviation.py & Percentiles.py.

--- Mean-Median-Mode.py
+++ Mean-Median-Mode.py
@@ -100,5 +100,4 @@ speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 x = stats.mode(speed)
 
 print(x)
-
 #The mode() method returns a ModeResult object that contains the mode number (86), and count (how many times the mode number appeared (3)).
